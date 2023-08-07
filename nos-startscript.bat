ce50333515032632a352caf06ce6ae1f48b80059
SoulofSorrow <julian.schaefer@xmail.dev>
2023-08-07 10:34:32 +0200

    Update nos-startscript.bat
    translate

--- nos-startscript.bat
+++ nos-startscript.bat
@@ -1,21 +1,21 @@
 @echo off
 set "GameServerPath=C:\gameserver\noonesurvivedserver\WRSHServer.exe"
 set "ServerOptions=-server -log -Port=8484 -QueryPort=45515"
-set "RestartDelay=10"  REM Verzögerung vor dem erneuten Starten in Sekunden (Anpassen nach Bedarf)
+set "RestartDelay=10"  REM Delay before restarting in seconds (Adjust as needed)
 
 :start_server
-echo Starte No One Survived Server...
+echo Starting No One Survived Server...
 start "" "%GameServerPath%" %ServerOptions%
 
-REM Überwache den Prozess in einer Schleife
+REM Monitor the process in a loop
 :monitor
 timeout /t %RestartDelay% /nobreak
 tasklist | find /i "%GameServerPath%" > nul
 
 if errorlevel 1 (
     echo.
-    echo Der Server-Prozess wurde beendet.
-    echo Starte den Server erneut...
+    echo The server process has been terminated.
+    echo Restarting the server...
     goto start_server
 ) else (
     goto monitor
